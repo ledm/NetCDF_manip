9ca5d76c5a9fb5c83c6eda05d887024e4f469eb6
Lee <ledm@pml.ac.uk>
2015-07-01 09:12:20 +0100

    Added BSD license to netcdf_manip library.

--- pruneNC.py
+++ pruneNC.py
@@ -1,3 +1,27 @@
+#
+# Copyright 2015, Plymouth Marine Laboratory
+#
+# Address:
+# Plymouth Marine Laboratory
+# Prospect Place, The Hoe
+# Plymouth, PL1 3DH, UK
+#
+# Email:
+# ledm@pml.ac.uk
+#
+# This file is part of the netcdf_manip library.
+#
+# netcdf_manip is free software: you can redistribute it and/or modify it
+# under the terms of the Revised Berkeley Software Distribution (BSD) 3-clause license. 
+#
+# netcdf_manip is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the revised BSD license for more details.
+# You should have received a copy of the revised BSD license along with netcdf_manip.
+# If not, see <http://opensource.org/licenses/BSD-3-Clause>.
+#
+
+
 
 #from ncdfView import ncdfView
 try:	from netCDF4 import Dataset, default_fillvals

8d7aef96cbd097a157363b5a240cb165f2bd1dfd
Lee de Mora <ledm@pml.ac.uk>
2017-08-24 16:16:17 +0100

    added setup files.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+MAJOR               = 0
+MINOR               = 1
+VERSION = '{}.{}'.format(MAJOR, MINOR)
+
+######
+# Use pip to install this script:
+# pip install -e . --user
+
+setup(name="netcdf_manip",
+      version=VERSION,
+      description="A repository to assist with manipulating netcdf files. Merging, pruning, renaming varaibles, creating 1D profiles from 3D files.",
+      author="Lee de Mora (PML)",
+      author_email="ledm@pml.ac.uk",
+      url="https://gitlab.ecosystem-modelling.pml.ac.uk/ledm/netcdf_manip",
+      license='Revised Berkeley Software Distribution (BSD) 3-clause license.',
+      classifiers=[
+          'Development Status :: 1 - Alpha',
+          'Intended Audience :: Scientific researcher',
+          'Topic :: NetCDF editing',
+          'License :: OSI Approved :: BSD3 License',
+          'Programming Language :: Python :: 2.7',
+          'Operating System :: UNIX',
+      ],
+      packages=["netcdf_manip"],
+      install_requires=[
+          "numpy",
+          "netCDF4", 
+          "datetime",
+          "os",
+          "operator",
+          "getpass",
+          "glob",
+      ],
+      )
+
+
